343715436c6d1997398d0b761f08ec2b3d8d6b3d
Daniela Hincapié González <dhincapig1@eafit.edu.co>
2024-08-20 17:13:40 -0500

    carrusel

--- src/app/components/pages/news/newsComponents/carousel/carousel.component.ts
+++ src/app/components/pages/news/newsComponents/carousel/carousel.component.ts
@@ -1,1 +1,35 @@
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import {
+  CarouselComponent,
+  CarouselControlComponent,
+  CarouselIndicatorsComponent,
+  CarouselInnerComponent,
+  CarouselItemComponent,
+  ThemeDirective
+} from '@coreui/angular';
 
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.scss'],
+  standalone: true,
+  imports: [ThemeDirective, CarouselComponent, CarouselIndicatorsComponent, CarouselInnerComponent, NgFor, CarouselItemComponent, CarouselControlComponent, RouterLink]
+})
+export class Carousel03Component implements OnInit {
+
+  slides: any[] = new Array(3).fill({ id: -1, src: '', title: '', subtitle: '' });
+
+  ngOnInit(): void {
+    this.slides[0] = {
+      src: './assets/img/angular.jpg'
+    };
+    this.slides[1] = {
+      src: './assets/img/react.jpg'
+    };
+    this.slides[2] = {
+      src: './assets/img/vue.jpg'
+    };
+  }
+}
